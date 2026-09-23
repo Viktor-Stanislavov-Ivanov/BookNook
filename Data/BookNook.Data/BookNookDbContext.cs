d6542c0c3ab78f4eeed4bf58ce498512b4e47fb9
Viktor <stu2401322041@uni-plovdiv.bg>
2026-09-24 00:42:27 +0300

    Create DbContext with restricted delete constraints

---
+++ Data/BookNook.Data/BookNookDbContext.cs
@@ -0,0 +1,37 @@
+﻿using BookNook.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookNook.Data
+{
+    public class BookNookContext : DbContext
+    {
+        public BookNookContext(DbContextOptions<BookNookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; } = null!;
+        public DbSet<Client> Clients { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<OrderLine> OrderLines { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Cannot delete a Client if they have Orders
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Client)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.ClientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Cannot delete a Book if it has been ordered (OrderLines)
+            modelBuilder.Entity<OrderLine>()
+                .HasOne(ol => ol.Book)
+                .WithMany()
+                .HasForeignKey(ol => ol.BookId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
