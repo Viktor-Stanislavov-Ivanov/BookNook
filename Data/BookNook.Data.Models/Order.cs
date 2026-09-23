17b229229e41facf15ab94a4d3424387701749ac
Viktor <stu2401322041@uni-plovdiv.bg>
2026-09-24 00:40:13 +0300

    Create Book, Client, Order and OrderLine entities, plus Enums, alongside all data annotations for all of them

---
+++ Data/BookNook.Data.Models/Order.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookNook.Data.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ClientId { get; set; }
+
+        [ForeignKey(nameof(ClientId))]
+        public Client Client { get; set; } = null!;
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public OrderStatus Status { get; set; }
+
+        [Required]
+        public DeliveryMethod DeliveryMethod { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalSum { get; set; }
+
+        public ICollection<OrderLine> OrderLines { get; set; } = new List<OrderLine>();
+    }
+}
---
+++ Data/BookNook.Data.Models/OrderLine.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookNook.Data.Models
+{
+    [PrimaryKey(nameof(OrderId), nameof(BookId))]
+    public class OrderLine
+    {
+        [Required]
+        public int OrderId { get; set; }
+
+        [ForeignKey(nameof(OrderId))]
+        public Order Order { get; set; } = null!;
+
+        [Required]
+        public int BookId { get; set; }
+
+        [ForeignKey(nameof(BookId))]
+        public Book Book { get; set; } = null!;
+
+        [Required]
+        [Range(1, 1000, ErrorMessage = "Quantity must be at least 1.")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal UnitPrice { get; set; }
+    }
+}
