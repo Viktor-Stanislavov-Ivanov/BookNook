fb9c6d41147753539f63b2378c63b8ea9f064d1a
Viktor <stu2401322041@uni-plovdiv.bg>
2026-09-26 10:00:00 +0300

    Add OrderFormViewModel and OrdersController

---
+++ Web/BookNook.Web/Controllers/OrdersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookNook.Data;
+using BookNook.Data.Models;
+using BookNook.Web.Models;
+
+namespace BookNook.Web.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly BookNookContext _context;
+
+        public OrdersController(BookNookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Orders/Create?bookId=5
+        public async Task<IActionResult> Create(int? bookId)
+        {
+            var viewModel = new OrderFormViewModel
+            {
+                Status = OrderStatus.New,
+                AvailableClients = await _context.Clients.OrderBy(c => c.Name).ToListAsync(),
+                AvailableBooks = await _context.Books.Where(b => b.AvailableQuantity > 0).OrderBy(b => b.Title).ToListAsync()
+            };
+
+            // if add to order is clicked pre-populate that book
+            if (bookId.HasValue)
+            {
+                var book = await _context.Books.FindAsync(bookId.Value);
+                if (book != null && book.AvailableQuantity > 0)
+                {
+                    viewModel.OrderLines.Add(new OrderLineViewModel
+                    {
+                        BookId = book.Id,
+                        BookTitle = book.Title,
+                        UnitPrice = book.Price,
+                        Quantity = 1,
+                        MaxQuantity = book.AvailableQuantity
+                    });
+                }
+            }
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Web/BookNook.Web/Models/OrderFormViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BookNook.Data.Models;
+
+namespace BookNook.Web.Models
+{
+    public class OrderFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please select a client.")]
+        [Display(Name = "Client")]
+        public int ClientId { get; set; }
+
+        [Required]
+        [Display(Name = "Delivery Method")]
+        public DeliveryMethod DeliveryMethod { get; set; }
+
+        [Required]
+        public OrderStatus Status { get; set; }
+
+        public List<OrderLineViewModel> OrderLines { get; set; } = new List<OrderLineViewModel>();
+
+        public IEnumerable<Client> AvailableClients { get; set; } = new List<Client>();
+        public IEnumerable<Book> AvailableBooks { get; set; } = new List<Book>();
+    }
+
+    public class OrderLineViewModel
+    {
+        public int BookId { get; set; }
+        public string BookTitle { get; set; } = string.Empty;
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public int MaxQuantity { get; set; }
+    }
+}
